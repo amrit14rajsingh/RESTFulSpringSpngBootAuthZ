073647c5bf84b9213d0818a512ab0c42e4ca4bb2
Amrit Raj <amrit.raj@kp.org>
2018-05-12 13:42:39 -0500

    This is to check-in new project and document it.

---
+++ src/test/java/com/example/restfulbyspring/RestFulSpringSpngBootAuthZApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.restfulbyspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestFulSpringSpngBootAuthZApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
